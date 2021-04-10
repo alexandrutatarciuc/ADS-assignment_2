6cefba0fb633cd00b16a762d7f4d590b033a936a
Ranteidz <55162517+Ranteidz@users.noreply.github.com>
2021-04-10 20:08:15 +0200

    bst

--- src/main/java/BinarySearchTree.java
+++ src/main/java/BinarySearchTree.java
@@ -9,7 +9,7 @@ public class BinarySearchTree extends BinaryTree {
     public BinarySearchTree(BinaryTreeNode node) {
         super.setRoot(node);
     }
-    
+
   public void insert(int element)
   {
 
@@ -59,9 +59,31 @@ public class BinarySearchTree extends BinaryTree {
 
   public void removeElement(int element)
   {
+    BinaryTreeNode tmpNode = null;
+    BinaryTreeNode foundNode = null;
 
     if (super.contains(element))
     {
+
+     tmpNode = super.getRoot();
+     foundNode = Search(tmpNode,element); // this need to return the parent of the element that needs to be deleted
+
+      // check if the element has kids
+
+     if(foundNode.getLeftChild() == null && foundNode.getRightChild()==null){
+       foundNode=null;
+     }
+     if(foundNode.getRightChild() == null){
+       foundNode.setElement(foundNode.getLeftChild().getElement());
+       BinaryTreeNode left = foundNode.getLeftChild();
+       left = null;
+     }
+
+     if(foundNode.getLeftChild() == null){
+       foundNode.setElement(foundNode.getRightChild().getElement());
+       BinaryTreeNode right = foundNode.getRightChild();
+       right = null;
+     }
 
         
 
@@ -122,5 +144,28 @@ public class BinarySearchTree extends BinaryTree {
 
       insert(i);
     }
+  }
+
+  private BinaryTreeNode Search(BinaryTreeNode node, int element){
+      if(node== null || node.getElement() == element){
+        return node;
+      }
+      if(node.getElement() < element){
+        return Search(node.getRightChild(), element);
+      }
+      return Search(node.getLeftChild(),element);
+  }
+
+  private BinaryTreeNode Parent(BinaryTreeNode node, int element){
+      if(node == null || node.getElement() == element){
+        return null;
+      }
+      if((node.getLeftChild()!=null && node.getLeftChild().getElement() == element) || (node.getRightChild() !=null && node.getRightChild().getElement()==element)){
+        return node;
+      }
+      BinaryTreeNode temp = Parent(node.getLeftChild(),element);
+      if(temp !=null){
+        return temp;
+      }
   }
 }
