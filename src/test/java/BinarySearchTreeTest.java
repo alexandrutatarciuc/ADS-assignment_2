1823da636798e6de83bb971fcd49ff91d2083aa7
Paul <bujor.paul2000@gmail.com>
2021-04-10 16:32:43 +0200

    test fix

--- src/test/java/BinarySearchTreeTest.java
+++ src/test/java/BinarySearchTreeTest.java
@@ -96,15 +96,16 @@ public class BinarySearchTreeTest {
         ArrayList<Integer> test = new ArrayList<>();
         test.add(4);
         test.add(2);
-        test.add(9);
+        test.add(8);
         test.add(1);
         test.add(3);
         test.add(6);
-        test.add(8);
+        test.add(9);
         test.add(5);
         test.add(7);
         test.add(10);
         assertArrayEquals(test.toArray(), tree.levelOrder().toArray());
         assertEquals(3, tree.height());
+        print.printTree(tree.getRoot());
     }
 }
